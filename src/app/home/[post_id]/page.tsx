828118e7a4eb1c11cda2578b5b5806f2c1d12345
Red1 <oradwane73@gmail.com>
2025-08-01 22:16:19 +0100

    page by post id

--- src/app/home/[post_id]/page.tsx
+++ src/app/home/[post_id]/page.tsx
@@ -1,16 +1,32 @@
 import DetailNav from "@/components/DetailNav";
-import React from "react";
+import PostCard from "@/components/PostCard";
+import { supabaseBrowser } from "@/supabase-utils/supabase-browser";
 
 const Page = async ({ params }: { params: Promise<{ post_id: string }> }) => {
   const { post_id } = await params;
+  const supabase = supabaseBrowser();
+  const { data, error } = await supabase
+    .from("posts")
+    .select("*")
+    .eq("id", post_id)
+    .single();
+
+  let content;
+  if (error) content = <div>Error loading post.</div>;
+  else if (!data) content = <div>Post not found.</div>;
+  else
+    content = (
+      <PostCard
+        post={{ ...data, image_url: data.image_url || data.imageURL || "" }}
+      />
+    );
+
   return (
     <div>
       <DetailNav />
-      {post_id}
+      {content}
     </div>
   );
 };
 
 export default Page;
-
-//
